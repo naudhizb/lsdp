bed2ae0e17e2f8cc5b622780feee2f03b6ed824e
ecourse <smkang@codenuri.co.kr>
2022-04-04 13:51:48 +0900

    Add files via upload

---
+++ DAY1/6_Edit1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
+
+class Edit
+{
+	string data;
+public:
+	string getData()
+	{
+		data.clear();
+
+		while (1)
+		{
+			char c = _getch();
+
+			if (c == 13) break;
+
+			if (isdigit(c))
+			{
+				cout << c << endl;
+				data.push_back(c);
+			}
+		}
+		cout << endl;
+		return data;
+	}
+};
+
+int main()
+{
+	Edit e;
+	while (1)
+	{
+		cout << e.getData() << endl;
+	}
+}
+
+
