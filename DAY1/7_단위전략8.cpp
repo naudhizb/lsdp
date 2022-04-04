98551da465e5b4db4c6feed3baadff928ac7d367
ecourse <smkang@codenuri.co.kr>
2022-04-04 16:06:37 +0900

    Add files via upload

--- DAY1/7_단위전략8.cpp
+++ DAY1/7_단위전략8.cpp
@@ -21,6 +21,7 @@ int main()
 }
 
 // STL set의 원리
+
 template<typename T>
 class set
 {
@@ -28,13 +29,14 @@ class set
 public:
 	void insert(const T& newElem)
 	{
+		// 아래처럼 만들면 무조건 크기 비교는 < 연산만 사용하게 됩니다
+		// 교체 불가능한 경직된 디자인
 		if (*root < newElem)
 			add right;
 		else if (newElem < *root)
 			add left;
 		else
 			fail;
-
 	}
 };
 
