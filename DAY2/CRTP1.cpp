365d535cbb5e564da9cde398b3e79b3c7bf9479b
ecourse <smkang@codenuri.co.kr>
2022-04-05 15:32:30 +0900

    Add files via upload

--- DAY2/CRTP1.cpp
+++ DAY2/CRTP1.cpp
@@ -1,5 +1,9 @@
+// CRTP1
 #include <iostream>
 
+// 이벤트 처리를 위해서 변하는 것을 가상함수로
+// MenuItem 의 경우 : 메뉴의 종류는 너무 많다. 파생 클래스 갯수가 많아진다.
+// MainWindow의경우 : MainWindow 는 갯수가 많지 않다. 나쁘지 않은 디자인
 class Window
 {
 public:
@@ -7,13 +11,18 @@ public:
 	{
 		// 발생한 이벤트 조사후 함수 호출
 		OnClick();
-		OnKeyDown();
+//		OnKeyDown();
 	}
-	void OnClick() {}
-	void OnKeyDown() {}
+	virtual void OnClick() {}
+	virtual void OnKeyDown() {}
 };
-
+class MainWindow : public Window
+{
+public:
+	void OnClick() override { std::cout << "MainWindow onClick" << std::endl;  }
+};
 int main()
 {
-
+	MainWindow mw;
+	mw.msgLoop();
 }
