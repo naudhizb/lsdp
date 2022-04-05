525d32906984ab38deba97fe4713580697c178cd
ecourse <smkang@codenuri.co.kr>
2022-04-05 15:59:14 +0900

    Add files via upload

--- DAY2/3_Command.cpp
+++ DAY2/3_Command.cpp
@@ -39,30 +39,92 @@ class AddRectCommand : public ICommand
 public:
 	AddRectCommand(std::vector<Shape*>& v) : v(v) {}
 
-	void Execute() override {}
+	void Execute() override { v.push_back(new Rect); }
+	bool CanUndo() override { return true; }
+	void Undo() override
+	{
+		Shape* p = v.back();
+		v.pop_back();
+		delete p;
+	}
 };
 
+class AddCircleCommand : public ICommand
+{
+	std::vector<Shape*>& v;   // 반드시 reference 로
+public:
+	AddCircleCommand(std::vector<Shape*>& v) : v(v) {}
+
+	void Execute() override { v.push_back(new Circle); }
+	bool CanUndo() override { return true; }
+	void Undo() override
+	{
+		Shape* p = v.back();
+		v.pop_back();
+		delete p;
+	}
+};
+
+class DrawCommand : public ICommand
+{
+	std::vector<Shape*>& v;   
+public:
+	DrawCommand(std::vector<Shape*>& v) : v(v) {}
+
+	void Execute() override 
+	{
+		for (auto p : v) p->Draw();
+	}
+
+	bool CanUndo() override { return true; }
+	void Undo() override
+	{
+		system("cls");
+	}
+};
+
+#include <stack>
 
 int main()
 {
 	std::vector<Shape*> v;
 
+	std::stack<ICommand*> cmd_stack;
+
+	ICommand* pCmd = 0;
+	
 	while (1)
 	{
 		int cmd;
 		std::cin >> cmd;
 		if (cmd == 1) 
 		{
-			v.push_back(new Rect);
+			pCmd = new AddRectCommand(v);
+			pCmd->Execute();
+			cmd_stack.push(pCmd);
 		}
 		else if (cmd == 2) 
 		{
-			v.push_back(new Circle);
+			pCmd = new AddCircleCommand(v);
+			pCmd->Execute();
+			cmd_stack.push(pCmd);
 		}
 		else if (cmd == 9)
 		{
-			for (auto p : v)
-				p->Draw();
+			pCmd = new DrawCommand(v);
+			pCmd->Execute();
+			cmd_stack.push(pCmd);
+		}
+		else if (cmd == 0)
+		{
+			if (!cmd_stack.empty())
+			{
+				pCmd = cmd_stack.top();
+				cmd_stack.pop();
+
+				if (pCmd->CanUndo())
+					pCmd->Undo();
+			}
 		}
 	}
 }
