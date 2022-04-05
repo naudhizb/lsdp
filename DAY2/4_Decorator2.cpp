91b3d2613961b7db81463e06ba131d56b3f8bf44
ecourse <smkang@codenuri.co.kr>
2022-04-05 13:11:11 +0900

    Add files via upload

--- DAY2/4_Decorator2.cpp
+++ DAY2/4_Decorator2.cpp
@@ -8,12 +8,18 @@ public:
 };
 
 // 상속을 사용한 기능추가
+
+// => 클래스에 기능을 추가하는 것
+// => 실행시간에 기능 추가가 아닌 코드 작성시 기능추가
+// SpaceShip ss 일때
+// ss에 기능을 추가한것은 아님.
 class NewSpaceShip : public SpaceShip
 {
 public:
 	void Fire() 
 	{	
 		SpaceShip::Fire(); // 기존 기능 사용
+
 		std::cout << "Fire Left Missile" << std::endl; // 새로운 기능추가
 	}
 };
