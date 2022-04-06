caae861d8f16e7b2571ab5716236c82951c2e739
ecourse <smkang@codenuri.co.kr>
2022-04-06 09:25:08 +0900

    Add files via upload

--- DAY3/1_Adapter5.cpp
+++ DAY3/1_Adapter5.cpp
@@ -15,7 +15,10 @@ public:
 	inline void pop()			  { c.pop_back(); }
 	inline T& top()			  { return c.back(); }
 };
-
+#include <stack> // C++ 표준의 stack이 위처럼 만들어져 있습니다
+				 // 흔히, stack adapter 입니다.
+				 // "클래스 어답터"로 만들어져 있는데.
+			     // 단위전략 기술로 "컨테이너 교체도 가능"
 int main()
 {
 //	stack<int, std::vector<int> > s; // vector를 stack 처럼 사용하게
