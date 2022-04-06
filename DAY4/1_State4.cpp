59297f2b3b48ebbda8e31e041335304c211e57d8
ecourse <smkang@codenuri.co.kr>
2022-04-07 08:55:06 +0900

    Add files via upload

--- DAY4/1_State4.cpp
+++ DAY4/1_State4.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 using namespace std;
 
+// 인터페이스 기반으로 변하는 것을 분리하는 패턴은 3가지 입니다.
+
+// 프로그램에서 사용하는 알고리즘의 교체 : strategy
+// 객체에 상태에 따른 동작의 교체 : state
+// 복잡한 객체를 만들때 공정과 세부 구현의 분리 : builder
+
+// state 패턴
+// 객체의 데이타는 변하는 않은데, 동작이 교체 된다.
+// 마치 새로운 클래스를 사용하는 것과 같다.
+
+
 // 변하는 동작을 다른 클래스로
 // 인터페이스 설계
 struct IState
@@ -34,10 +45,12 @@ struct GoldItem : public IState
 int main()
 {
 	NoItem ni; GoldItem gi;
+
 	Charater* p = new Charater;
 	p->setState(&ni);
 	p->run();
 	p->attack();
+
 	p->setState(&gi);
 	p->run();
 }
