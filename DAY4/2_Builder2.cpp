cb2ebb873504346dca0e84e039d8e953e12f186f
ecourse <smkang@codenuri.co.kr>
2022-04-07 09:14:21 +0900

    Add files via upload

--- DAY4/2_Builder2.cpp
+++ DAY4/2_Builder2.cpp
@@ -3,6 +3,18 @@
 #include <string>
 using namespace std;
 
+// Builder : 복잡한 객체를 생성하는데..
+//			 동일한 공정을 사용하고, 각 공정의 표현이 다른 객체를 생성할때 사용
+
+// 생성패턴 5가지 정리
+// singleton      : 오직 한개의 객체만 생성 
+// prototype      : 기존 객체를 복사해서 새로운 객체 생성
+// abstract factory : 제품의 군을 생성하기 위한 인터페이스 설계
+// factory method : 객체를 만들기 위한 인터페이스 정의, 어떤 객체를 만들지는 하위 클래스가 결정 
+// builder        : 동일 공정으로 복잡한 객체 만들기.
+
+
+
 // 축구게임 캐릭터를 생각해 봅시다.
 // 모자, 유니폼, 신발등을 변경할수 있습니다.
 typedef string Hat;			// class Hat{}
@@ -28,7 +40,7 @@ public:
 	{
 		// 캐릭터를 만드는 공정은 동일 합니다.
 		Character c;
-		c = c + pBuilder->makeHat();
+	//	c = c + pBuilder->makeHat();
 		c = c + pBuilder->makeUniform();
 		c = c + pBuilder->makeShoes();
 		return c;
