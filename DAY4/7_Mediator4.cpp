9dd45739ca6a9158be4f22e26f28345e598f3e7b
ecourse <smkang@codenuri.co.kr>
2022-04-07 14:58:14 +0900

    Add files via upload

--- DAY4/7_Mediator4.cpp
+++ DAY4/7_Mediator4.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <vector>
+using namespace std::placeholders;
 
 class NotificationCenter
 {
@@ -19,16 +20,44 @@ public:
 		for (auto f : notif_map[key])
 			f(hint);
 	}
+
+	// global 통보센터라는 개념도 만들어 봅시다.
+	static NotificationCenter& defaultCenter()
+	{
+		static NotificationCenter instance;
+		return instance;
+	}
 };
 //-----------------------
 void foo(void* p) { std::cout << "foo" << std::endl; }
 void goo(void* p) { std::cout << "goo" << std::endl; }
 
+class Machine
+{
+public:
+	void work1(void* p)
+	{
+
+	}
+	void work2(void* p, int a)
+	{
+
+	}
+
+
+};
 int main()
 {
 	// IOS 개발의 핵심의 "통보센터" 입니다.
-	NotificationCenter nc;
+//	NotificationCenter nc;
+
+	// 공동의 작업은 아래처럼 사용하자 라고 약속
+	NotificationCenter& nc = NotificationCenter::defaultCenter();
 	
+	Machine m;
+	nc.RegisterObserver("LOWBATTERY", std::bind(&Machine::work1, &m, _1) );
+	nc.RegisterObserver("LOWBATTERY", std::bind(&Machine::work2, &m, _1, 5));
+
 	nc.RegisterObserver("LOWBATTERY", foo);
 	nc.RegisterObserver("LOWBATTERY", goo);
 
